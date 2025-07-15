6f3fdc1ca60ff806cd50b8145bfa567e037cf0c3
brmcbrid <69523108+brmcbrid@users.noreply.github.com>
2025-07-15 12:03:16 -0400

    Initial Commit

--- Lab7.cpp
+++ Lab7.cpp
@@ -1,18 +1,8 @@
-// Write a program that prompts the user to enter a multiple 
-// digit positive integer then use a loop to iteratively construct 
-// the value that is the reverse of that number, add the two numbers together, 
-// and print the reverse value and sum to the screen. Note: Be sure that your 
-// solution is not simply printing the digits of the number in reverse. 
-// For example, if the number 462 was entered the your program will find 
-// the reverse number is 264 and the sum of those two values is 1050.
-
-
-// Place you solution code for the Find the Reverse of a Multi-digit program here
-
 #include <iostream>
 using namespace std;
 
 int main() {
+    // Place you solution code for the Find the Reverse of a Multi-digit program here
 
     return 0;
 }
